cb2cb98adc0a3f703ae64cd62890fd0cba3e5f8a
mmirk <mmirk@expediagroup.com>
2024-12-02 08:33:04 +0100

    feat: interpret all arguments as the text input

--- application/src/main/kotlin/Application.kt
+++ application/src/main/kotlin/Application.kt
@@ -7,11 +7,13 @@ import text.spi.TextPlugin
 import java.util.ServiceLoader
 
 fun main(args: Array<String>) {
-    if (args.isEmpty() || args[0].isEmpty()) {
-        println("Usage: text '<text>'")
+    if (args.isEmpty()) {
+        println("Usage: text <text>")
+        println("You can pass a text including spaces, " +
+            "but if there are special characters, you should quote it.")
         return
     }
-    val text = args[0]
+    val text = args.joinToString(" ")
     val plugins =
         (listOf(EchoPlugin()) + ServiceLoader.load(TextPlugin::class.java))
             .associateBy { it.name }
